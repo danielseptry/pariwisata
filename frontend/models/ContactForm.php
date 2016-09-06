b7fc6b81f3754b5d5605a150aa6cf89bed1ad071
danielseptry <septrypanjaitan@gmail.com>
2016-09-06 18:14:40 +0700

    Update ContactForm.php

--- frontend/models/ContactForm.php
+++ frontend/models/ContactForm.php
@@ -3,7 +3,7 @@
 namespace frontend\models;
 
 https://github.com/danielseptry/pariwisata.gituse Yii;
- use yii\base\Model;
+use yii\base\Model;
 
 /**
  * ContactForm is the model behind the contact form.

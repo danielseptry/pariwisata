804ecacbb4c6cd4bf14c6d3627dad235a3de63dd
danielseptry <septrypanjaitan@gmail.com>
2016-09-06 17:50:14 +0700

    Update main.php

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -20,7 +20,7 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>

6a96fcf6e84abf12022c723d435c57e6aece533d
danielseptry <septrypanjaitan@gmail.com>
2016-09-06 18:17:07 +0700

    Update ContactForm.php

--- frontend/models/ContactForm.php
+++ frontend/models/ContactForm.php
@@ -8,7 +8,7 @@ use yii\base\Model;
 /**
  * ContactForm is the model behind the contact form.
  */
-class ContactForm extends Model
+ class ContactForm extends Model
 {
     public $name;
     public $email;

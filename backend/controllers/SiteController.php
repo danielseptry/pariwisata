468f1d4b9f51b04056d09a7b947d3d4f37025cbb
danielseptry <septrypanjaitan@gmail.com>
2016-09-06 15:49:14 +0700

    Update SiteController.php

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -22,7 +22,7 @@ class SiteController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['login', 'error'],
+                        'a ctions' => ['login', 'error'],
                         'allow' => true,
                     ],
                     [

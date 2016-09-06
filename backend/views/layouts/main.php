1a635bad2dd7f69af26e7fcc911dd565dfb984f4
danielseptry <septrypanjaitan@gmail.com>
2016-09-06 17:41:17 +0700

    Create main.php

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -5,7 +5,7 @@
 
 use backend\assets\AppAsset;
 use yii\helpers\Html;
- use yii\bootstrap\Nav;
+use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
@@ -45,35 +45,3 @@ AppAsset::register($this);
             . Html::submitButton(
                 'Logout (' . Yii::$app->user->identity->username . ')',
                 ['class' => 'btn btn-link']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; Pariwisata Sumatera Utara <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
-
-<?php $this->endBody() ?>
-</body>
-</html>
-<?php $this->endPage() ?>

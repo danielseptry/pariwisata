e894a9140d9ef0ffbcd0a51a6db858f604b563a3
danielseptry <septrypanjaitan@gmail.com>
2016-09-06 17:40:29 +0700

    Update main.php

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -5,7 +5,7 @@
 
 use backend\assets\AppAsset;
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
+ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;

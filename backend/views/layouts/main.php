b3303707c2fb0bbd4ffa784d34519e10ed984315
danielseptry <septrypanjaitan@gmail.com>
2016-09-06 17:52:07 +0700

    Update main.php

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -21,7 +21,7 @@ AppAsset::register($this);
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
-</head>
+ </head>
 <body>
 <?php $this->beginBody() ?>
 

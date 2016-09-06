34983d50457a83ac76eb0a2ea4275790bebefe9d
danielseptry <septrypanjaitan@gmail.com>
2016-09-06 18:11:09 +0700

    Update _form.php

--- frontend/views/tempat/_form.php
+++ frontend/views/tempat/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-     <?= $form->field($model, 'nama_tempat')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'nama_tempat')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'mlat')->textInput(['maxlength' => true]) ?>
 

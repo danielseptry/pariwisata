dc5045e05d9765d1ecafee6938d386c4c96c1fbf
danielseptry <septrypanjaitan@gmail.com>
2016-09-06 17:03:52 +0700

    Update SiteController.php

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -4,7 +4,7 @@ namespace backend\controllers;
 use Yii;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
-use yii\filters\AccessControl ;
+use yii\filters\AccessControl;
 use common\models\LoginForm;
 
 /**
